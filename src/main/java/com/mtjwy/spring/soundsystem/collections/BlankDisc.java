ccc7536cab0de70a2b86a28a0b7b38b459a59801
Yu Wu <wuyucsu@gmail.com>
2015-10-10 14:49:29 -0500

    finish Constructor Arg Collection Junit Test

---
+++ src/main/java/com/mtjwy/spring/soundsystem/collections/BlankDisc.java
@@ -0,0 +1,24 @@
+package com.mtjwy.spring.soundsystem.collections;
+
+import java.util.List;
+
+import com.mtjwy.spring.soundsystem.CompactDisc;
+
+public class BlankDisc implements CompactDisc {
+	private String title;
+	private String artist;
+	private List<String> tracks;
+
+	public BlankDisc(String title, String artist, List<String> tracks) {
+		this.title = title;
+		this.artist = artist;
+		this.tracks = tracks;
+	}
+
+	public void play() {
+		System.out.println("Playing " + title + " by " + artist);
+		for (String track : tracks) {
+			System.out.println("-Track: " + track);
+		}
+	}
+}
